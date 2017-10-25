da99077d31b1006239536351ae222e978c3b4f52
Kyrell Dixon <kyrell.brown@gmail.com>
2017-10-25 17:47:00 -0500

    Fixed errors on getName test

--- src/test_scoop.cpp
+++ src/test_scoop.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 int main() {
-    Scoop vanilla("vanilla", "for those who like to keep it classy", 2.0, 5.0, 30);
+    Scoop vanilla("Vanilla", "for those who like to keep it classy", 2.0, 5.0, 30);
 
     if (vanilla.getName() != "Vanilla") {
-      cerr << "Vanilla test failed: got " << vanilla.getName() << endl;
+      std::cerr << "Vanilla test failed: got " << vanilla.getName() << std::endl;
       return 1;
     }
 
